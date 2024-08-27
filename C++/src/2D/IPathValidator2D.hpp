e4a407f3bc30ecd8c40ab829ee141b04eb6da00a
Andrew Campbell <acc2023@comcast.net>
2024-08-27 10:13:48 -0500

    Preliminary work

--- C++/src/2D/IPathValidator2D.hpp
+++ C++/src/2D/IPathValidator2D.hpp
@@ -5,11 +5,13 @@
 // Eigen
 #include <Eigen/Dense>
 
+#include "Util/CustomTypes/VectorAndPointTypes.hpp"
+
 namespace Algorithms::TwoD
 {
     class IPathValidator2D
     {
         public:
-            virtual bool validatePath(std::deque<)
+            virtual bool validatePath(std::deque<StateXYT>);
     };
 }
